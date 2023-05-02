b4e4483a3400bcae2245cfadafc05209d9f76641
Joe Tay <tay.joe@gmail.com>
2023-05-02 15:01:35 +0800

    Update main.py
    Remove wiki tool from agent bing and google

--- main.py
+++ main.py
@@ -82,7 +82,7 @@ def main():
 
 
 	#st.title("✎ Metacognition prototype V1.1")
-	st.title("GPT3.5/4 Dialogic Agent prototype V1.2")
+	st.title("GPT3.5/4 Dialogic Agent prototype V1.3")
 	#st.title("✎ CherGpt - Virtual Learning Assistant (Beta V2)")
 	#st.title("Sourcefinder prototype")
 
